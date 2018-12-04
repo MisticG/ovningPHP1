ac8fe43bca38cf6912e746e4919ea9bf9579a936
tuyvi <tuy-vi.huynh@medieinstitutet.se>
2018-12-04 11:12:54 +0100

    adding 1 in for loop

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+        $greetings = "Hello World";
+        echo("<h1>" . $greetings . "</h1>");
+
+        echo ("<br>" . "<br>");
+
+        
+        for($x = 0; $x<=10; $x++) {
+            echo "number is $x <br>";
+        };
+
+        echo "<br>";
+
+        //lägger till 1 till varje $number med hjälp av for loop
+
+        $number = 1;
+        for($number = 1; $number<=10; $number++) {
+            echo "number is $number <br>";
+        };
+
+        
+
+    
+    ?>
+    
+</body>
+</html>
